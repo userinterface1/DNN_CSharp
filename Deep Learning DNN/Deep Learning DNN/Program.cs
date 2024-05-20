682479e266067f14298ed27904bced2da1d3d598
userinterface1 <89734982+userinterface1@users.noreply.github.com>
2024-05-20 21:29:07 +0900

    Added Batching
    배칭추가

--- Deep Learning DNN/Deep Learning DNN/Program.cs
+++ Deep Learning DNN/Deep Learning DNN/Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace Deep_Learning_DNN
 {
+    using System;
+    using System.IO;
+
     internal class Program
     {
+        static float[] targets1, targets2;
+        static float[] inputs1, inputs2;
+        static int batchSize;
+
         static float target1, target2;
 
         static float input1, input2;
@@ -14,9 +21,9 @@
 
         static float learning_rate;
 
-        static float epoch;
+        static int epoch;
 
-        static void propagation()
+        static void propagation(float input1, float input2, float target1, float target2)
         {
             float z1 = input1 * w1 + input2 * w2;
             h1 = sigmoid(z1);
@@ -36,91 +43,66 @@
             total = e1 + e2;
         }
 
-        static void backpropagation()
+        static void backpropagation(float input1, float input2, float target1, float target2)
         {
             float totaldoutput1 = -(target1 - output1);
-
             float o1dz3 = output1 * (1 - output1);
-
             float z3dw5 = h1;
-
             float totaldw5 = totaldoutput1 * o1dz3 * z3dw5;
-            float update_w5 = w5 - learning_rate * totaldw5;
 
             float z3dw6 = h2;
             float totaldw6 = totaldoutput1 * o1dz3 * z3dw6;
-            float update_w6 = w6 - learning_rate * totaldw6;
 
             float totaldoutput2 = -(target2 - output2);
-
             float o2dz3 = output2 * (1 - output2);
-
             float z4dw7 = h1;
-
             float totaldw7 = totaldoutput2 * o2dz3 * z4dw7;
-            float update_w7 = w7 - learning_rate * totaldw7;
 
             float z4dw8 = h2;
-
             float totaldw8 = totaldoutput2 * o2dz3 * z4dw8;
-            float update_w8 = w8 - learning_rate * totaldw8;
 
             float e1doutput1 = -(target1 - output1);
-
             float output1dz3 = output1;
-
             float z3dh1 = w5;
-
             float e1dh1 = e1doutput1 * output1dz3 * z3dh1;
 
             float e2doutput2 = -(target2 - output2);
-
             float output2dz4 = output2;
-
             float z4dh1 = w7;
-
             float e2dh1 = e2doutput2 * output2dz4 * z4dh1;
 
             float totaldh1 = e1dh1 + e2dh1;
 
-            //
             float z3dh2 = h2;
-
             float e1dh2 = e1doutput1 * output1dz3 * z3dh2;
 
             float z4dh2 = h2;
-
             float e2dh2 = e2doutput2 * output2dz4 * z4dh2;
 
             float totaldh2 = e1dh2 + e2dh2;
 
-            //
             float h1dz1 = h1 * (1 - h1);
             float z1dw1 = input1;
             float totaldw1 = totaldh1 * h1dz1 * z1dw1;
-            float update_w1 = w1 - learning_rate * totaldw1;
 
             float z1dw2 = input2;
             float totaldw2 = totaldh1 * h1dz1 * z1dw2;
-            float update_w2 = w2 - learning_rate * totaldw2;
 
             float h2dz2 = h2 * (1 - h2);
             float z2dw3 = input1;
             float totaldw3 = totaldh2 * h2dz2 * z2dw3;
-            float update_w3 = w3 - learning_rate * totaldw3;
 
-            float z2dw4 = output2;
+            float z2dw4 = input2;
             float totaldw4 = totaldh2 * h2dz2 * z2dw4;
-            float update_w4 = w4 - learning_rate * totaldw4;
 
-            w1 = update_w1;
-            w2 = update_w2;
-            w3 = update_w3;
-            w4 = update_w4;
-            w5 = update_w5;
-            w6 = update_w6;
-            w7 = update_w7;
-            w8 = update_w8;
+            w1 -= learning_rate * totaldw1;
+            w2 -= learning_rate * totaldw2;
+            w3 -= learning_rate * totaldw3;
+            w4 -= learning_rate * totaldw4;
+            w5 -= learning_rate * totaldw5;
+            w6 -= learning_rate * totaldw6;
+            w7 -= learning_rate * totaldw7;
+            w8 -= learning_rate * totaldw8;
         }
 
         static float sigmoid(float z)
@@ -139,10 +121,25 @@
             string yorn = Console.ReadLine();
             if (yorn == "y")
             {
-                Console.Write("Target 1 : ");
-                target1 = float.Parse(Console.ReadLine());
-                Console.Write("Target 2 : ");
-                target2 = float.Parse(Console.ReadLine());
+                Console.Write("Batch Size: ");
+                batchSize = int.Parse(Console.ReadLine());
+
+                targets1 = new float[batchSize];
+                targets2 = new float[batchSize];
+                inputs1 = new float[batchSize];
+                inputs2 = new float[batchSize];
+
+                for (int i = 0; i < batchSize; i++)
+                {
+                    Console.Write($"Target 1 for batch {i + 1}: ");
+                    targets1[i] = float.Parse(Console.ReadLine());
+                    Console.Write($"Target 2 for batch {i + 1}: ");
+                    targets2[i] = float.Parse(Console.ReadLine());
+                    Console.Write($"Input 1 for batch {i + 1}: ");
+                    inputs1[i] = float.Parse(Console.ReadLine());
+                    Console.Write($"Input 2 for batch {i + 1}: ");
+                    inputs2[i] = float.Parse(Console.ReadLine());
+                }
 
                 Console.Write("Learning Rate : ");
                 learning_rate = float.Parse(Console.ReadLine());
@@ -170,16 +167,14 @@
                     load_weight();
                 }
 
-                Console.Write("Input 1 : ");
-                input1 = float.Parse(Console.ReadLine());
-                Console.Write("Input 2 : ");
-                input2 = float.Parse(Console.ReadLine());
-
                 for (int i = 0; i < epoch; ++i)
                 {
-                    propagation();
-                    display();
-                    backpropagation();
+                    for (int j = 0; j < batchSize; ++j)
+                    {
+                        propagation(inputs1[j], inputs2[j], targets1[j], targets2[j]);
+                        display();
+                        backpropagation(inputs1[j], inputs2[j], targets1[j], targets2[j]);
+                    }
                 }
 
                 Console.WriteLine("W1 : " + w1);
@@ -202,7 +197,7 @@
                 Console.Write("Input 2 : ");
                 input2 = float.Parse(Console.ReadLine());
 
-                propagation();
+                propagation(input1, input2, target1, target2);
                 display();
             }
         }
